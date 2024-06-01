e2aa0d2cede1d4117d5858ea1cdd1e02bcdc1011
vvihorev <vvihorevv@gmail.com>
2024-06-01 11:48:35 +0200

    Update user password via /users/{id}

--- routes.py
+++ routes.py
@@ -185,6 +185,8 @@ async def update_user(id: str, user: models.UpdateUserModel = Body(...)):
     user_dict = {
         k: v for k, v in user.model_dump(by_alias=True).items() if v is not None
     }
+    if "password" in user_dict:
+        user_dict["password"] = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
 
     if len(user_dict) >= 1:
         update_result = await users_collection.find_one_and_update(
